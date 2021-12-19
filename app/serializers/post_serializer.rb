bd837dad64368dcd8f9feca5a3c475a030d97202
David Avery <eismausen@gmail.com>
2021-12-18 21:04:41 -0800

    commit tree 5fa409137b9725f23a5741e661a968232c7c3a6b

---
+++ app/serializers/post_serializer.rb
@@ -0,0 +1,10 @@
+class PostSerializer < ActiveModel::Serializer
+  attributes :title, :content, :short_content
+  belongs_to :author
+  has_many :tags, through: :post_tags, serializer: TagSerializer
+   
+
+  def short_content    
+    "#{self.object.content[0..39]}..."
+  end  
+end
